d177b2a753ce78405bee9b26afa4b2caed098502
Preecha <preecha.sayang@gmail.com>
2025-09-03 15:14:08 +0700

    add/feat:assig-2-addmorefunction

--- server/app.mjs
+++ server/app.mjs
@@ -45,3 +45,104 @@ app.post("/assignments", async (req, res)=>{
   
 })
 
+app.get("/assignments", async(req,res)=>{
+  try{
+    const result= await connectionPool.query(
+      `SELECT * FROM assignments`
+    )
+  return res.status(200).json({"message": "showdata",
+     data: result.rows,
+  })
+  }catch(e){
+    return res.status(500).json( { "message": "Server could not read assignment because database connection" })
+  }
+})
+
+
+
+
+
+
+app.get("/assignments/:assignmentId", async (req,res)=>{
+  
+  const assignid = req.params.assignmentId
+  
+      if(!assignid){
+      return res.status(404).json({"message": "Server could not find a requested assignment"})
+    }
+
+
+
+  try{
+    const result = await connectionPool.query(
+      `SELECT * FROM assignments where assignment_id = $1`,[assignid]
+    )
+    return res.status(200).json({data: result.rows})
+    
+  }catch(e){
+    return res.status(500).json( { "message": "Server could not read assignment because database connection" })
+  }
+
+})
+
+
+
+
+
+
+
+app.delete("/assignments/:assignmentId", async (req,res)=>{
+  try{
+    const asignid= req.params.assignmentId
+    if(!asignid){
+      res.status(404).json({"message": "Server could not find a requested assignment to delete"})
+    }
+    
+
+    const result = await connectionPool.query(
+      `DELETE  FROM assignments where assignment_id= $1`,
+      [asignid])
+    res.status(200).json( { "message": "Deleted assignment sucessfully" })
+    
+  }catch(e){
+    return res.status(500).json( { "message": "Server could not read assignment because database connection" })
+  }
+})
+
+
+
+
+
+app.put("/assignments/:assignmentId", async (req,res)=>{
+  try{
+    const asignid= req.params.assignmentId
+    const update = {...req.body, updated_at: new Date()
+    }
+
+
+    if(!asignid){
+     return res.status(404).json({"message": "Server could not find a requested assignment to delete"})
+    }
+
+    const result = await connectionPool.query(
+      `UPDATE assignments
+      SET title=$1,
+          content=$2,
+          category=$3,
+          updated_at=$4
+      WHERE assignment_ID=$5
+      `,[
+        update.title,
+        update.content,
+        update.category,
+        update.updated_at,
+        asignid
+      ]
+    )
+
+    return res.status(200).json({"message": "Updated assignment sucessfully"})
+
+  }catch(e){
+    return res.status(500).json( { "message": "Server could not read assignment because database connection" })
+  }
+})
